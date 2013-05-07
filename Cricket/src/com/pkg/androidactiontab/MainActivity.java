bedbf05e4a05edb90d64f2f90038e231e6eb4a0c
wade <wade@Phenom>
2013-05-07 16:38:59 +0100

    cricket

---
+++ Cricket/src/com/pkg/androidactiontab/MainActivity.java
@@ -0,0 +1,56 @@
+package com.pkg.androidactiontab;
+
+
+
+import android.os.Bundle;
+
+
+import android.app.Activity;
+import android.app.TabActivity;
+
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+
+public class MainActivity extends TabActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// Access Tab Host from Main Layout
+		   TabHost tab_host = (TabHost)findViewById(android.R.id.tabhost);
+		   // Set Tab Specification for Battery Tab
+		   
+	        TabSpec battery_tab_spec = tab_host.newTabSpec("battery_tab");
+	        
+	        battery_tab_spec.setContent(R.id.battery_tab);
+	        battery_tab_spec.setIndicator("Battery");
+	        tab_host.addTab(battery_tab_spec);
+	        
+	        // Set Tab Specification for Network Tab
+	        TabSpec network_tab_spec = tab_host.newTabSpec("network_tab");
+	        
+	        network_tab_spec.setContent(R.id.network_tab);
+	        network_tab_spec.setIndicator("Network");
+	        tab_host.addTab(network_tab_spec);
+	        
+	        // Set Tab Specification for Device Tab
+	        TabSpec  device_tab_spec = tab_host.newTabSpec("device_tab");
+	        
+	        device_tab_spec.setContent(R.id.device_tab);
+	        device_tab_spec.setIndicator("Device");
+	        tab_host.addTab(device_tab_spec);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
