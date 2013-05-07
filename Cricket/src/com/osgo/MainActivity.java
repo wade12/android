847ef2e464db209a818958912ba8bb1cb89aae0e
wade <wade@Phenom>
2013-05-07 16:58:51 +0100

    update

--- Cricket/src/com/osgo/MainActivity.java
+++ Cricket/src/com/osgo/MainActivity.java
@@ -1,22 +1,22 @@
-package com.pkg.androidactiontab;
+package com.osgo;
 
-
+import com.osgo.R;
 
 import android.os.Bundle;
 
 
 import android.app.Activity;
 import android.app.TabActivity;
-
 import android.view.Menu;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
 
-public class MainActivity extends TabActivity {
-	
+public class MainActivity extends TabActivity
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// Access Tab Host from Main Layout
@@ -26,31 +26,33 @@ public class MainActivity extends TabActivity {
 	        TabSpec battery_tab_spec = tab_host.newTabSpec("battery_tab");
 	        
 	        battery_tab_spec.setContent(R.id.battery_tab);
-	        battery_tab_spec.setIndicator("Battery");
+	        battery_tab_spec.setIndicator("Off");
 	        tab_host.addTab(battery_tab_spec);
 	        
 	        // Set Tab Specification for Network Tab
 	        TabSpec network_tab_spec = tab_host.newTabSpec("network_tab");
 	        
 	        network_tab_spec.setContent(R.id.network_tab);
-	        network_tab_spec.setIndicator("Network");
+	        network_tab_spec.setIndicator("Middle");
 	        tab_host.addTab(network_tab_spec);
 	        
 	        // Set Tab Specification for Device Tab
 	        TabSpec  device_tab_spec = tab_host.newTabSpec("device_tab");
 	        
 	        device_tab_spec.setContent(R.id.device_tab);
-	        device_tab_spec.setIndicator("Device");
+	        device_tab_spec.setIndicator("Leg");
 	        tab_host.addTab(device_tab_spec);
-		
-		
-	}
+	        
+	} // end method onCreate
+	
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
+		
+	} // end method onCreateOptionsMenu
 
-}
+} // end Class MainActivity
